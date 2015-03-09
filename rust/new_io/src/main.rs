06fb2ae5a5ab58c1709453de3ae4f20ec77c2cda
Jonathan Almeida <git@jonalmeida.com>
2015-03-08 22:47:24 -0400

    Tried to learn some builder patterns

--- rust/new_io/src/main.rs
+++ rust/new_io/src/main.rs
@@ -27,17 +27,6 @@ use std::path::Path;
 fn main() {
     let path = Path::new("test_file.txt");
     let path2 = Path::new("test_file2.txt");
-    /*
-    let mut options = OpenOptions::new();
-    options.read(true);
-    let file = match options.open(&path.clone()) {
-        Ok(file) => file,
-        Err(..)  => panic!("Flop like a fish!"),
-    };*/
-
-//    let file = File::open("test_file.txt").unwrap();
-
-    let path = Path::new("test_file.txt");
 
     // or we can also just use a string slice:
     let path2 = "test_file2.txt";
@@ -49,22 +38,4 @@ fn main() {
     // Both of these should be valid
     let file: Result<File, Error> = options.open(path);
     let file2: Result<File, Error> = options.open(path2);
-    /*
-    let file2 = match options.open(&path2.clone()) {
-        Ok(file) => file,
-        Err(..)  => panic!("Drop everything. Shits hit the fan!"),
-    };
-    */
-
-    //let file_result: Result<File, Error> = options.open(&path.clone());
-    /*
-    let mut reader = BufReader::new(&file);
-    let buffer_string = &mut String::new();
-    reader.read_line(buffer_string);
-    match reader.read_line(buffer_string) {
-        Ok(result) => result,
-        Err(e)     => panic!("at the disco"),
-    };
-    println!("We read in one line: {}", buffer_string);
-    */
 }
